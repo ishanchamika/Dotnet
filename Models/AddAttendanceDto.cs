83d2f0bf0c20fd1bc747725d45e5fcc4fe813487
ishanchamika <ishanchami9@gmail.com>
2025-02-09 22:56:24 +0530

    attendance dto

---
+++ Models/AddAttendanceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Register.Models
+{
+    public class AddAttendanceDto
+    {
+        public required Guid UserId { get; set; }
+        public DateTime? CameTime { get; set; }
+        public DateTime? LeftTime { get; set; }
+        public bool IsUpdated { get; set; } = false;
+        public string? Reason { get; set; }
+    }
+}
