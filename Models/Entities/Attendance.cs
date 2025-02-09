6a7b482703031085a603d539b3aa652e82f7f41a
ishanchamika <ishanchami9@gmail.com>
2025-02-09 22:56:01 +0530

    create attendance Entity with related into Signup Entity(Id column is related as UserId in Attendance table)

---
+++ Models/Entities/Attendance.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Register.Models.Entities
+{
+    public class Attendance
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+        public DateTime CameTime { get; set; } = DateTime.UtcNow;
+        public DateTime LeftTime { get; set; } = DateTime.UtcNow;
+        public bool IsUpdated { get; set; } = false;
+        public string? Reason { get; set; }
+
+        [ForeignKey("UserId")]
+        public Signup? User { get; set; }
+    }
+}
