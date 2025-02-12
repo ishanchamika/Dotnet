ad6d7f66a3d859f8edc0f34b1854d3ae3f3fd97b
ishanchamika <ishanchami9@gmail.com>
2025-02-12 22:01:47 +0530

    creat Add task entity and dto

---
+++ Models/AddTasksDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Register.Models
+{
+    public class AddTasksDto
+    {
+        public Guid UserId { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public string TaskStatus { get; set; }
+        public string TaskDeadline { get; set; }
+    }
+}
---
+++ Models/Entities/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Register.Models.Entities
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public string ? TaskName { get; set; }
+        public string ? TaskDescription { get; set; }
+        public string ? TaskStatus { get; set; }
+        public string ? TaskDeadline { get; set; }
+
+        [ForeignKey("UserId")]
+        public Signup? User { get; set; }
+    }
+}
