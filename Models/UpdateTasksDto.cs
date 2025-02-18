7a1cc8eb5f072e7f9a7688bf7bf7d05da15ae968
ishanchamika <ishanchami9@gmail.com>
2025-02-18 23:24:01 +0530

    UpdateTasksDto

---
+++ Models/UpdateTasksDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Register.Models
+{
+    public class UpdateTasksDto
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public string ? TaskName { get; set; }
+        public string ? TaskDescription { get; set; }
+        public string ? TaskStatus { get; set; }
+        public string ? TaskDeadline { get; set; }
+    }
+}
