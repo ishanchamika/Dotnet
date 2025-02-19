c232a222994211a9163f15e8b0fa4690cbb82145
ishanchamika <ishanchami9@gmail.com>
2025-02-19 23:50:13 +0530

    send message

--- Models/Entities/ChatHub.cs
+++ Models/Entities/ChatHub.cs
@@ -2,11 +2,16 @@
 
 namespace Register.Models.Entities
 {
-    public sealed class ChatHub : Hub
+    public sealed class ChatHub : Hub<IChatClient>
     {
         public override async Task OnConnectedAsync()
         {
-            await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} has joined");
+            await Clients.All.ReceiveMessage($"{Context.ConnectionId} has joined");
+        }
+
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.ReceiveMessage($"{Context.ConnectionId}: message");
         }
     }
 }
