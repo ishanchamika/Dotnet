7bb56ebca081416284c38fdfa29fc92ff4d190b4
ishanchamika <ishanchami9@gmail.com>
2025-02-19 22:59:08 +0530

    create ChatHyb class (check using Postman's web socket request)

---
+++ Models/Entities/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Register.Models.Entities
+{
+    public sealed class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} has joined");
+        }
+    }
+}
