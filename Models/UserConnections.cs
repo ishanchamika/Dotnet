4ff7f0618fefbdb19ea87634c9480eac6b2be440
ishanchamika <ishanchami9@gmail.com>
2025-02-22 23:04:05 +0530

    Add userConnections Model

---
+++ Models/UserConnections.cs
@@ -0,0 +1,8 @@
+﻿namespace Register.Models
+{
+    public class UserConnections
+    {
+        public string Username { get; set; } = string.Empty;
+        public string ChatRoom { get; set; } = string.Empty;
+    }
+}
