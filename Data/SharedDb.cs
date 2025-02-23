8af82c54ada41ef4e1647ef05b8a558b7a0285d5
ishanchamika <ishanchami9@gmail.com>
2025-02-23 21:43:12 +0530

    added SharedDb class

---
+++ Data/SharedDb.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Concurrent;
+using Register.Models;
+
+namespace Register.Data
+{
+    public class SharedDb
+    {
+        private readonly ConcurrentDictionary<string, UserConnections> _connections =  new ();
+        public ConcurrentDictionary<string, UserConnections> connections => _connections;
+    }
+}
