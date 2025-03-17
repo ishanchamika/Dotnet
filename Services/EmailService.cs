a89962e3500f1700a357c6861b9682f4ada25f03
ishanchamika <ishanchami9@gmail.com>
2025-03-17 23:59:14 +0530

    email service

---
+++ Services/EmailService.cs
@@ -0,0 +1,11 @@
+﻿namespace Register.Services
+{
+    public class EmailService
+    {
+        private readonly IConfiguration _config;
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+    }
+}
