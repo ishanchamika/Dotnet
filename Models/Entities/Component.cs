42f1f77a5b7ebd1080e71ddfe6422ad679f01877
ishanchamika <ishanchami9@gmail.com>
2025-03-26 22:57:41 +0530

    store html(angular) component for relavant questions

---
+++ Models/Entities/Component.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Register.Models.Entities
+{
+    public class Component
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
