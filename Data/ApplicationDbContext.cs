e5db69a3cad2c42d4509edaf0a05d78c0172bc1a
ishanchamika <ishanchami9@gmail.com>
2025-03-26 22:58:10 +0530

    define table names

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,6 +13,33 @@ namespace Register.Data
         public DbSet<Signup> Signups { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
         public DbSet<Tasks> Tasks { get; set; }
+        public DbSet<Component> Components { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<QuestionMap> QuestionMaps { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Ensuring the foreign key relationship for QuestionMap -> Question
+            modelBuilder.Entity<QuestionMap>()
+                .HasOne(qm => qm.Question)
+                .WithMany()  // No navigation property in Question
+                .HasForeignKey(qm => qm.Current_qid)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Ensuring the foreign key relationship for Question -> Component
+            modelBuilder.Entity<Question>()
+                .HasOne(q => q.Component)
+                .WithMany()
+                .HasForeignKey(q => q.ComponentId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Ensuring the foreign key relationship for QuestionMap -> Component
+            modelBuilder.Entity<QuestionMap>()
+                .HasOne(qm => qm.Component)
+                .WithMany()
+                .HasForeignKey(qm => qm.ComponentId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
 
