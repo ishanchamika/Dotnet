559a52ad067de701ff6d408dd2191afe953ca3db
ishanchamika <ishanchami9@gmail.com>
2025-03-26 22:55:29 +0530

    create Question table for store the questions

---
+++ Models/Entities/Question.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Register.Models.Entities
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Current_qid { get; set; }  // Consider whether this is needed
+
+        [ForeignKey("Component")]
+        public int ComponentId { get; set; }
+
+        public string Text { get; set; }  // Renamed from `Questions` to `Text`
+        public string Note { get; set; }
+
+        public Component Component { get; set; }
+    }
+}
