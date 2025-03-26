a227e51bfc152554a51b713ebcd686bcffa2c938
ishanchamika <ishanchami9@gmail.com>
2025-03-26 22:56:11 +0530

    related to QUestions and COmponents tables

---
+++ Models/Entities/QuestionMap.cs
@@ -0,0 +1,22 @@
+﻿using Register.Models.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+public class QuestionMap
+{
+    [Key]
+    public int Id { get; set; }
+    public string Note { get; set; }
+
+    [ForeignKey("Question")]
+    public int Current_qid { get; set; }
+
+    public int Next_qid { get; set; }
+    public int Prev_qid { get; set; }
+
+    [ForeignKey("Component")]
+    public int ComponentId { get; set; }
+
+    public Question Question { get; set; }
+    public Component Component { get; set; }
+}
