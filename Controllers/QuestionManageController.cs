e8c9e97e18997102cf7103cd717e0c4b0a76ef1d
ishanchamika <ishanchami9@gmail.com>
2025-03-26 22:59:02 +0530

    created Controller due to questions manage

---
+++ Controllers/QuestionManageController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Register.Data;
+
+namespace Register.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionManageController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IConfiguration configuration;
+
+        public QuestionManageController(ApplicationDbContext _dbContext, IConfiguration configuration)
+        {
+            this._dbContext = _dbContext;
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetQuestionsRange(int id)
+        {
+            return Ok();
+        }
+    }
+}
