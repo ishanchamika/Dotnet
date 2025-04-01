adf0ad369fd6f373af16363e6bf0f42edad1d184
ishanchamika <ishanchami9@gmail.com>
2025-04-01 23:39:57 +0530

    updated

--- Models/Entities/Question.cs
+++ Models/Entities/Question.cs
@@ -7,15 +7,12 @@ namespace Register.Models.Entities
     {
         [Key]
         public int Id { get; set; }
-
-        public int Current_qid { get; set; }  // Consider whether this is needed
-
-        [ForeignKey("Component")]
+        public int Current_qid { get; set; }
         public int ComponentId { get; set; }
+        public string? Text { get; set; }
+        public string? Note { get; set; }
 
-        public string Text { get; set; }  // Renamed from `Questions` to `Text`
-        public string Note { get; set; }
-
-        public Component Component { get; set; }
+        //[ForeignKey("ComponentId")]
+        //public Component? Component { get; set; }
     }
 }
